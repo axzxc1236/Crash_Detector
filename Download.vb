bb2b9beb8d7b7e72746074a88ca12e70f7d281b1
axzxc1236 <axzxc1236@gmail.com>
2014-06-07 17:49:43 +0800

    Update Download.vb

--- Download.vb
+++ Download.vb
@@ -1,4 +1,4 @@
-﻿Imports System.IO
+Imports System.IO
 
 Public Class Download
     Private Sub Download_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -10,7 +10,13 @@ Public Class Download
         If Not My.Computer.FileSystem.DirectoryExists(My.Computer.FileSystem.CurrentDirectory & "\Crash-Detector") Then My.Computer.FileSystem.CreateDirectory(My.Computer.FileSystem.CurrentDirectory & "\Crash-Detector")
         If Not My.Computer.FileSystem.FileExists(Environment.CurrentDirectory & "\Crash-Detector\7za.exe") Then My.Computer.Network.DownloadFile("https://dl.dropboxusercontent.com/s/07mw2d3o2v8xkq2/7za.exe", Environment.CurrentDirectory & "\Crash-Detector\7za.exe", "", "", False, 100000, True)
         If Not My.Computer.FileSystem.FileExists(Environment.CurrentDirectory & "\Crash-Detector\curl.exe") Then
-            My.Computer.Network.DownloadFile("http://www.paehl.com/open_source/?download=curl_737_0_ssl.zip", Environment.CurrentDirectory & "\Crash-Detector\curl.zip", "", "", False, 100000, True)
+            Try
+                My.Computer.Network.DownloadFile("http://www.paehl.com/open_source/?download=curl_737_0_ssl.zip", Environment.CurrentDirectory & "\Crash-Detector\curl.zip", "", "", False, 100000, True)
+            Catch ex As System.Net.WebException
+                My.Computer.Clipboard.SetText("例外狀況(01)-CURL_NOT_FOUND")
+                MsgBox("請用留言通知作者" & vbCrLf & "例外狀況(01)-CURL_NOT_FOUND" & vbCrLf & "我會盡快釋出更正後的版本" & vbCrLf & "內容已經自動複製，謝謝合作，程式終止")
+                End
+            End Try
             My.Computer.FileSystem.WriteAllText(Environment.CurrentDirectory & "\Crash-Detector\unzip.bat", "cd Crash-Detector" & vbCrLf & "cd Crash-Detector" & vbCrLf & "7za.exe e curl.zip", False)
 
             Shell("cmd /c Crash-Detector\unzip.bat", AppWinStyle.Hide, True)
